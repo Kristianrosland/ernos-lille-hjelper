d76adf3f9d621affceb5e4459f07445dc4d9364b
Kristian Rosland <kristian.rosland@gmail.com>
2020-04-04 14:22:26 +0200

    Initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import cases from "./cases";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [query, setQuery] = useState("");
+  const [currentCases, setCurrentCases] = useState([]);
+
+  const onChange = (e) => {
+    const inp = e.target.value.toUpperCase();
+    setQuery(inp);
+
+    if (inp.length === 3) {
+      const matches = cases.filter((c) => c.name.startsWith(inp));
+      setCurrentCases(matches);
+      setQuery("");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <input value={query} onChange={onChange} className={"query-input"} />
+      {currentCases.map((c) => (
+        <img key={c.name} src={c.src} className={"image"} alt={"test"} />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/cases.js
@@ -0,0 +1,66 @@
+import UL1a from "./cases/UL1a.png";
+import UR1b from "./cases/UR1b.png";
+import UU4b from "./cases/UU4b.png";
+import UL5a from "./cases/UL5a.png";
+import DD1a from "./cases/DD1a.png";
+import UR5a from "./cases/UR5a.png";
+import next from "./cases/DD1a.png";
+import next1 from "./cases/DD1a.png";
+import next2 from "./cases/DD1a.png";
+import next3 from "./cases/DD1a.png";
+import next4 from "./cases/DD1a.png";
+
+export default [
+  {
+    src: UL1a,
+    name: "UL1a",
+  },
+  {
+    src: UR1b,
+    name: "UR1b",
+  },
+  {
+    src: UU4b,
+    name: "UU4b",
+  },
+  {
+    src: UL5a,
+    name: "UL5a",
+  },
+  {
+    src: DD1a,
+    name: "DD1a",
+  },
+  {
+    src: UR5a,
+    name: "UR5a",
+  },
+  {
+    src: next,
+    name: "next",
+  },
+  {
+    src: next,
+    name: "next",
+  },
+  {
+    src: next,
+    name: "next",
+  },
+  {
+    src: next4,
+    name: "next",
+  },
+  {
+    src: next3,
+    name: "next",
+  },
+  {
+    src: next2,
+    name: "next",
+  },
+  {
+    src: next1,
+    name: "next",
+  },
+];
