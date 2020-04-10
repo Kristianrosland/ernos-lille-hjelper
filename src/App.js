e0fca0e8f42b3adae91b079c366349ee917380e0
Kristian Rosland <kristian.rosland@gmail.com>
2020-04-10 22:46:55 +0200

    useState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { HashRouter, Route } from 'react-router-dom';
 import Algorithms from './algorithms/Algorithms';
 
