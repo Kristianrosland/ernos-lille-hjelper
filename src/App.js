8bd425ca79bbabe2df66d50771bf68d16da3a2e6
Kristian Rosland <kristian.rosland@gmail.com>
2020-04-10 20:50:22 +0200

    Endre til less

--- src/App.js
+++ src/App.js
@@ -1,40 +1,11 @@
-import React, { useState } from "react";
-import "./App.css";
+import React from "react";
 import Search from "./Search";
-import Quiz from "./Quiz";
-
-const PlayMode = {
-  SEARCH: "SEARCH",
-  QUIZ: "QUIZ",
-};
+import css from "./app.less";
 
 const App = () => {
-  const [playMode, setPlayMode] = useState(PlayMode.QUIZ);
-
   return (
-    <div className="container">
-      <div className="play-mode-selection">
-        <span
-          className={`${playMode === PlayMode.SEARCH ? "selected" : ""}`}
-          onClick={() => setPlayMode(PlayMode.SEARCH)}
-        >
-          Søk
-        </span>
-        {" | "}
-        <span
-          className={`${playMode === PlayMode.QUIZ ? "selected" : ""}`}
-          onClick={() => setPlayMode(PlayMode.QUIZ)}
-        >
-          Quiz
-        </span>
-      </div>
-
-      {playMode === PlayMode.SEARCH && (
-        <Search setQuizMode={() => setPlayMode(PlayMode.QUIZ)} />
-      )}
-      {playMode === PlayMode.QUIZ && (
-        <Quiz setSearchMode={() => setPlayMode(PlayMode.SEARCH)} />
-      )}
+    <div className={css.container}>
+      <Search />
     </div>
   );
 };
