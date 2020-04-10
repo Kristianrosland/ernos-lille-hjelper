5265f069cb54a851fd0a267f898b49085f73e027
mathilde.warstad <mathildewaerstad@gmail.com>
2020-04-10 21:17:47 +0200

    Merge med master

--- src/Algorithms.js
+++ src/Algorithms.js
@@ -1,17 +1,17 @@
 import React, { useState } from "react";
+import css from "./algorithms.less";
 import cases from "./cases";
-import css from "./app.less";
 
 const Algorithms = () => {
   const [query, setQuery] = useState("");
   const [currentCases, setCurrentCases] = useState([]);
 
-  const onChange = (e) => {
+  const onChange = e => {
     const inp = e.target.value.toUpperCase();
     setQuery(inp);
 
     if (inp.length === 3) {
-      const matches = cases.filter((c) => c.name.startsWith(inp));
+      const matches = cases.filter(c => c.name.startsWith(inp));
       setCurrentCases(matches);
       setQuery("");
     }
@@ -20,7 +20,7 @@ const Algorithms = () => {
   return (
     <div className={css.container}>
       <input value={query} onChange={onChange} className={css.queryInput} />
-      {currentCases.map((c) => (
+      {currentCases.map(c => (
         <img key={c.name} src={c.src} className={css.image} alt="Solution" />
       ))}
     </div>
