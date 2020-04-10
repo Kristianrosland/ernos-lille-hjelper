c6b36375e2065c2bd12e60ea7f8c0aee8347e0e8
Kristian Rosland <kristian.rosland@gmail.com>
2020-04-10 22:23:43 +0200

    Added setting for auto import fix

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
-import React, { useState, useEffect } from "react";
-import { HashRouter, Route } from "react-router-dom";
-import Algorithms from "./algorithms/Algorithms";
+import React, { useEffect } from 'react';
+import { HashRouter, Route } from 'react-router-dom';
+import Algorithms from './algorithms/Algorithms';
 
-import firebase from "firebase/app";
-import config from "./firebase.config";
-import "firebase/auth";
+import firebase from 'firebase/app';
+import config from './firebase.config';
+import 'firebase/auth';
 firebase.initializeApp(config);
 let auth;
 
 export const AuthContext = React.createContext(null);
 
 const App = () => {
-  const [authState, setAuthState] = useState({ user: null, isLoading: true });
+    const [authState, setAuthState] = useState({ user: null, isLoading: true });
 
-  /* For å bruke auth i en annen komponent:
+    /* For å bruke auth i en annen komponent:
         import { AuthContext } from "../App";
         const auth = useContext(AuthContext);  // { user, isLoading }
-   */
+    */
 
-  useEffect(() => {
-    if (!auth) {
-      auth = firebase.auth();
+    useEffect(() => {
+        if (!auth) {
+            auth = firebase.auth();
 
-      firebase.auth().onAuthStateChanged((user) => {
-        setAuthState({ user, isLoading: false });
-      });
-    }
-  }, []);
+            firebase.auth().onAuthStateChanged((user) => {
+                setAuthState({ user, isLoading: false });
+            });
+        }
+    }, []);
 
-  return (
-    <AuthContext.Provider value={authState}>
-      <HashRouter>
-        <Route exact path="/" component={Algorithms} />
-        <Route path="/algorithms" component={Algorithms} />
-      </HashRouter>
-    </AuthContext.Provider>
-  );
+    return (
+        <AuthContext.Provider value={authState}>
+            <HashRouter>
+                <Route exact path="/" component={Algorithms} />
+                <Route path="/algorithms" component={Algorithms} />
+            </HashRouter>
+        </AuthContext.Provider>
+    );
 };
 
 export default App;
