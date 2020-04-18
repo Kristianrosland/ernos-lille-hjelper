f80e089c4b36ce2b84fce2c7a606d255b2e89dda
mathilde.warstad <mathildewaerstad@gmail.com>
2020-04-19 01:23:31 +0200

    legge til stats for tre av fem

--- src/cube-timer/SessionStats.tsx
+++ src/cube-timer/SessionStats.tsx
@@ -10,8 +10,11 @@ const findAverageSolve = (solves: Solve[]): number => {
     return totalSolveTime / solves.length;
 };
 
+const sumOfSolves = (solves: Solve[]): number =>
+    solves.reduce((solveTimeSum, solve) => solveTimeSum + solve.time, 0);
+
 const findBestAverageSolveOfFive = (solves: Solve[]): number => {
-    let firstSumOfFive = solves.slice(0, 5).reduce((solveTimeSum, solve) => solveTimeSum + solve.time, 0);
+    let firstSumOfFive = sumOfSolves(solves.slice(0, 5));
     let bestSumOfFive = firstSumOfFive;
 
     for (let i = 0; i < solves.length - 5; i++) {
@@ -22,6 +25,31 @@ const findBestAverageSolveOfFive = (solves: Solve[]): number => {
     }
 
     return bestSumOfFive / 5;
+};
+
+const getThreeOfFiveSum = (solves: Solve[]) =>
+    sumOfSolves(solves) - findBestSolve(solves) - findWorstSolve(solves);
+
+const findLastThreeOfFiveAverageSolve = (solves: Solve[]) => {
+    const lastFiveSolves = solves.slice(0, 5);
+
+    return getThreeOfFiveSum(lastFiveSolves) / 3;
+};
+
+const findBestThreeOfFiveAverageSolve = (solves: Solve[]): number => {
+    const firstFiveSolves = solves.slice(0, 5);
+    const firstThreeOfFive = getThreeOfFiveSum(firstFiveSolves);
+
+    let bestThreeOfFive = firstThreeOfFive;
+
+    for (let i = 0; i < solves.length - 5; i++) {
+        const fiveSolves = solves.slice(i, i + 5);
+        const sum = getThreeOfFiveSum(fiveSolves);
+
+        bestThreeOfFive = Math.min(sum, bestThreeOfFive);
+    }
+
+    return bestThreeOfFive / 3;
 };
 
 interface Props {
@@ -36,6 +64,10 @@ const SessionStats: React.FC<Props> = ({ sessionSolves, bestSolve }) => {
 
     const bestAverageOfFiveSolveTime =
         sessionSolves.length >= 5 ? findBestAverageSolveOfFive(sessionSolves) : null;
+    const lastThreeOfFiveAverageSolveTime =
+        sessionSolves.length >= 5 ? findLastThreeOfFiveAverageSolve(sessionSolves) : null;
+    const bestThreeOfFiveAverageSolveTime =
+        sessionSolves.length >= 5 ? findBestThreeOfFiveAverageSolve(sessionSolves) : null;
 
     return (
         <>
@@ -44,6 +76,13 @@ const SessionStats: React.FC<Props> = ({ sessionSolves, bestSolve }) => {
                 <div>beste: {sessionSolves.length > 0 ? formatTimer(bestSolveTime) : '-'}</div>
                 <div>dårligte: {sessionSolves.length > 1 ? formatTimer(worstSolveTime) : '-'}</div>
                 <div>gjennomsnitt: {sessionSolves.length > 1 ? formatTimer(averageSolveTime) : '-'}</div>
+                <div>
+                    3 av 5: {lastThreeOfFiveAverageSolveTime ? formatTimer(lastThreeOfFiveAverageSolveTime) : '-'}
+                </div>
+                <div>
+                    beste 3 av 5:{' '}
+                    {bestThreeOfFiveAverageSolveTime ? formatTimer(bestThreeOfFiveAverageSolveTime) : '-'}
+                </div>
                 <div>
                     beste gjennomsnitt av 5:{' '}
                     {bestAverageOfFiveSolveTime ? formatTimer(bestAverageOfFiveSolveTime) : '-'}
@@ -62,6 +101,18 @@ const SessionStats: React.FC<Props> = ({ sessionSolves, bestSolve }) => {
                     <div className={css.statLabel}>dårligste:</div>
                     <div className={css.statValue}>
                         {sessionSolves.length > 1 ? formatTimer(worstSolveTime) : '-'}
+                    </div>
+                </div>
+                <div className={css.statElement}>
+                    <div className={css.statLabel}>3 av 5:</div>
+                    <div className={css.statValue}>
+                        {lastThreeOfFiveAverageSolveTime ? formatTimer(lastThreeOfFiveAverageSolveTime) : '-'}
+                    </div>
+                </div>
+                <div className={css.statElement}>
+                    <div className={css.statLabel}>beste 3 av 5:</div>
+                    <div className={css.statValue}>
+                        {bestThreeOfFiveAverageSolveTime ? formatTimer(bestThreeOfFiveAverageSolveTime) : '-'}
                     </div>
                 </div>
                 <div className={css.statElement}>
