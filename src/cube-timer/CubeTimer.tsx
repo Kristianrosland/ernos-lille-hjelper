866f731596430f8ad98f25fc16cd90a81aab7243
Kristian Rosland <kristian.rosland@gmail.com>
2020-04-20 17:43:01 +0200

    Scramble som URL-parameter

--- src/cube-timer/CubeTimer.tsx
+++ src/cube-timer/CubeTimer.tsx
@@ -1,4 +1,5 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
 import scrambleGenerator from 'rubiks-cube-scramble';
 import { DataContext } from '../FirebaseProvider';
 import LoginLink from '../LoginLink';
@@ -8,25 +9,40 @@ import Scramble from './Scramble';
 import SessionStats from './SessionStats';
 import Timer from './Timer';
 
-const newScramle = (): string => scrambleGenerator() as string;
+const newScramble = (): string => (scrambleGenerator() as string).trim();
 
 const CubeTimer = () => {
+    const params = useParams<{ scramble: string }>();
     const [timerRunning, setTimerRunning] = useState(false);
-    const [scramble, setScramble] = useState<string>(newScramle());
     const { sessionSolves, stored, addNewSolve } = useContext(DataContext);
+
+    const history = useHistory();
+
+    const setNewScramble = () => {
+        history.push('/' + newScramble().replace(/ /g, '-'));
+    };
+
+    useEffect(() => {
+        if (params.scramble === undefined) {
+            setNewScramble();
+        }
+    }, []);
 
     return (
         <div className={css.cubeTimerContainer}>
             <LoginLink />
-            {!timerRunning && <Scramble newScramble={scramble} />}
+            {!timerRunning && <Scramble newScramble={params.scramble ? params.scramble.replace(/-/g, ' ') : ''} />}
 
             <Timer
                 timerRunning={timerRunning}
                 onToggleTimerRunning={setTimerRunning}
-                onNewScramble={() => setScramble(newScramle)}
+                onNewScramble={() => setNewScramble()}
                 solves={sessionSolves}
-                addSolve={solveTime => addNewSolve({ time: solveTime, timestamp: now(), scramble })}
+                addSolve={solveTime =>
+                    addNewSolve({ time: solveTime, timestamp: now(), scramble: params.scramble })
+                }
             />
+
             {!timerRunning && <SessionStats sessionSolves={sessionSolves} bestSolve={stored.best} />}
         </div>
     );
